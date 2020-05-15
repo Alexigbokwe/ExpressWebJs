d11210c738075c810d3c1e08212f092760e07f24
chukwuemekaigbokwe <juvitafrica@gmail.com>
2020-05-15 10:19:06 +0100

    Modified Auth middleware

--- App/Http/Middleware/Auth.js
+++ App/Http/Middleware/Auth.js
@@ -1,10 +1,24 @@
 const HttpError = require("@middleware/HttpError");
+require("dotenv").config();
 Auth = (req, res, next) => {
-  console.log("this is the authentication middleware");
-  var isAuth = true;
-  if (isAuth) {
+  const header = req.headers["authorization"];
+  if (typeof header !== "undefined") {
+    const bearer = header.split(" ");
+    const token = bearer[1];
+
+    jwt.verify(token, process.env.APP_KEY, (err, authorizedData) => {
+      if (err)
+        return res
+          .status(500)
+          .send({ auth: false, message: "Failed to authenticate token." });
+      res.json({
+        message: "Successful log in",
+        authorizedData,
+      });
+    });
     next();
   } else {
+    //If header is undefined return Forbidden (403)
     res.json(new HttpError("Authentication field", 404));
   }
 };
