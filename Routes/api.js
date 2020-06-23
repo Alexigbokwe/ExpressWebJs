cfedf7fd57b6611f01b55cac87012696d567e206
chukwuemekaigbokwe <juvitafrica@gmail.com>
2020-06-23 09:44:34 +0100

    route clean up

--- Routes/api.js
+++ Routes/api.js
@@ -1,6 +1,5 @@
 "use strict";
 const { Route, at } = require("@router");
-//const at = require("@controllerBox");
 /*
     |--------------------------------------------------------------------------
     | Api route   
