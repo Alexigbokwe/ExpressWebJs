6338228ed0eecc6610cfacc060000902b587bc53
Alex Igbokwe <juvitafrica@gmail.com>
2021-02-07 02:44:08 +0100

    hash clean up

--- App/Config/hashing.js
+++ App/Config/hashing.js
@@ -8,7 +8,7 @@ module.exports = {
     | passwords for your application. By default, the bcrypt algorithm is
     | used; however, you remain free to modify this option if you wish.
     |
-    | Supported: "bcrypt", "argon".
+    | Supported: "bcrypt".
     |
     */
 
@@ -27,24 +27,5 @@ module.exports = {
 
   bcrypt: {
     rounds: process.env.BCRYPT_ROUNDS || 10,
-  },
-
-  /*
-    |--------------------------------------------------------------------------
-    | Argon Options
-    |--------------------------------------------------------------------------
-    |
-    | Here you may specify the configuration options that should be used when
-    | passwords are hashed using the Argon algorithm. These will allow you
-    | to control the amount of time it takes to hash the given password.
-    |
-    */
-
-  argon: {
-    hashLength: 32,
-    timeCost: 3,
-    memory: 4096,
-    parallelism: 1,
-    type: "argon2i",
   },
 };
