cf0cc02308922d897c124c2cb31efac6818cf2fc
Alex Igbokwe <juvitafrica@gmail.com>
2021-03-31 12:39:12 +0100

    feature: utility
    Add HttpResponse

--- Bootstrap/Queue/SendQueue.js
+++ Bootstrap/Queue/SendQueue.js
@@ -1,37 +1,37 @@
 "use strict";
-const checkDriver = Symbol("checkDriver");
-const dispatchViaRabbitmq = Symbol("dispatchViaRabbitmq");
-
 class SendQueue {
   static processQueue(job, queue, connection) {
     let jobObject = { job, queue, connection };
-    this[checkDriver](jobObject);
+    this.#checkDriver(jobObject);
   }
 
-  static [checkDriver](jobObject) {
+  static #checkDriver(jobObject) {
     switch (jobObject.connection.driver) {
       case "rabbitmq":
-        this[dispatchViaRabbitmq](jobObject);
+        this.#dispatchViaRabbitmq(jobObject);
         break;
       default:
         break;
     }
   }
 
-  static async [dispatchViaRabbitmq](jobObject) {
-    let connect = null;
+  static #dispatchViaRabbitmq(jobObject) {
     try {
-      connect = require("amqplib");
+      var amqp = require('amqplib/callback_api');
+      amqp.connect(
+        `${jobObject.connection.host}:${jobObject.connection.port}`,function(err,connect){
+           if(err) throw err;
+           connect.createChannel((error1,channel)=>{
+            if(error1) throw error1;
+            channel.assertQueue(jobObject.queue);
+            let queueMessage = JSON.stringify(jobObject.job);
+            channel.sendToQueue(jobObject.queue, Buffer.from(queueMessage));
+           })
+        }
+      );
     } catch (error) {
       throw error;
     }
-    connect = await require("amqplib").connect(
-      `${jobObject.connection.host}:${jobObject.connection.port}`,
-    );
-    const channel = await connect.createChannel();
-    await channel.assertQueue(jobObject.queue);
-    const queueMessage = JSON.stringify(jobObject.job);
-    channel.sendToQueue(jobObject.queue, Buffer.from(queueMessage));
   }
 }
 
