4e17c861e42e80de72488329ca5dfe6cc4672bc2
Alex Igbokwe <juvitafrica@gmail.com>
2021-04-21 23:03:35 +0100

    cleanup

--- Database/Migrations/20201209124747_user.js
+++ Database/Migrations/20201209124747_user.js
@@ -4,16 +4,17 @@
  * Remember, it's always in plural
  */
 let tableName = "users";
-exports.up = function (knex) {
-  return knex.schema.createTable(tableName, (table) => {
+exports.up = function (migration) {
+  return migration.schema.createTable(tableName, (table) => {
     table.increments("id");
-    table.string("first_name");
-    table.string("last_name");
-    table.string("email");
+    table.string("first_name").notNullable();
+    table.string("last_name").notNullable();
+    table.string("email").notNullable().unique();
+    table.string("password").notNullable();
     table.timestamps(true, true);
   });
 };
 
-exports.down = function (knex) {
+exports.down = function (migration) {
   return knex.schema.dropTable(tableName);
 };
